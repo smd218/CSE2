ac102d67ec56cbedd60d2b07db2675e4857e40d3
smd218 <smd218@lehigh.edu>
2015-01-26 20:44:12 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////
+//Sophie Davis
+//CSE2 - HW01
+//Prof. Brian Chen
+//1-27-15
+//Welcome Class Java Program
+//
+//  first compile program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  Define a class
+public class WelcomeClass{
+
+//  add main method
+        public static void main(String[ ] args) {
+            
+//  print statement
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-S--M--D--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("Hello! I am Sophie and I am taking CSE2 as a requirement for my major, cognitive science. Artificial intellegence is very interesting to me.");
+    
+    }
+}
